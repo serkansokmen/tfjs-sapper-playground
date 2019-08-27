af19e74ea9419bbfef26a28c034b63fff34fa8b5
Serkan Sokmen <e.serkan.sokmen@gmail.com>
2019-08-27 15:40:22 +0300

    Added spinner to loading component

--- src/components/Loading.svelte
+++ src/components/Loading.svelte
@@ -3,8 +3,22 @@
   export let label = 'Loading';
 </script>
 
+<style>
+  .loading {
+    display: grid;
+    align-items: center;
+    justify-items: start;
+    grid-template-columns: 0.25fr 1.75fr;
+    grid-template-rows: 0.25fr 1.75fr;
+    grid-gap: 1em;
+  }  
+</style>
+
 {#if loading}
+<div class="loading">
+  <img height="12em" src="images/spinner.svg" alt="Loading spinner"/>
   <span>{label}</span>
+</div>
 {:else}
   <slot />
 {/if}
